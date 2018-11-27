64892bf5e45c16522e05452c8ad97815bf09f217
Max Bruckner <max@maxbruckner.de>
2018-11-27 21:26:06 +0100

    Implement and switch to DescriptionComponentsParser

--- src/description_component.rs
+++ src/description_component.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Error;
+use parsers::*;
 
 #[derive(PartialEq)]
 #[derive(Debug)]
@@ -13,62 +14,136 @@ pub enum DescriptionComponent {
     KeyValue(String, String),
 }
 
+pub type DescriptionComponents = Vec<DescriptionComponent>;
+
+
+pub struct ProjectParser {}
+
+impl Parser for ProjectParser {
+    type Value = DescriptionComponent;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        lazy_static! {
+            static ref PROJECT_REGEX : Regex = Regex::new(r"^\+(?P<project>[^\s]+)").expect("Regex is invalid");
+        }
+
+        let captures = PROJECT_REGEX.captures(input)?;
+        Some(ParserResult {
+            value: DescriptionComponent::Project(String::from(&captures[1])),
+            remaining: &input[captures[0].len()..],
+        })
+    }
+}
+
+pub struct ContextParser {}
+
+impl Parser for ContextParser {
+    type Value = DescriptionComponent;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        lazy_static! {
+            static ref CONTEXT_REGEX : Regex = Regex::new(r"^@(?P<context>[^\s]+)").expect("Regex is invalid");
+        }
+
+        let captures = CONTEXT_REGEX.captures(input)?;
+        Some(ParserResult {
+            value: DescriptionComponent::Context(String::from(&captures[1])),
+            remaining: &input[captures[0].len()..],
+        })
+    }
+}
+
+pub struct KeyValueParser {}
+
+impl Parser for KeyValueParser {
+    type Value = DescriptionComponent;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        lazy_static! {
+            static ref KEY_VALUE_REGEX : Regex = Regex::new(r"^(?P<key>[^\s:]+):(?P<value>[^\s:]+)").expect("Regex is invalid");
+        }
+
+        let captures = KEY_VALUE_REGEX.captures(input)?;
+        Some(ParserResult {
+            value: DescriptionComponent::KeyValue(String::from(&captures[1]), String::from(&captures[2])),
+            remaining: &input[captures[0].len()..],
+        })
+    }
+}
+
+pub struct NormalTextParser {}
+
+impl Parser for NormalTextParser {
+    type Value = DescriptionComponent;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        lazy_static!{
+            static ref NORMAL_TEXT_REGEX : Regex = Regex::new(r"^(?P<text>[^@\+]*?\s|^\s*)(?:(?:@|\+)|[^\s@\+]+:)[^s]").expect("Regex is invalid");
+        }
+
+        match NORMAL_TEXT_REGEX.captures(input) {
+            Some(captures) => if captures[1].is_empty() {
+                    None
+                } else {
+                    Some(ParserResult {
+                        value: DescriptionComponent::Text(String::from(&captures[1])),
+                        remaining: &input[captures[1].len()..],
+                    })
+                },
+            None => if input.is_empty() {
+                    None
+                } else {
+                    Some(ParserResult {
+                        value: DescriptionComponent::Text(String::from(input)),
+                        remaining: "",
+                    })
+                }
+        }
+    }
+}
+
+pub struct DescriptionComponentParser {}
+
+impl Parser for DescriptionComponentParser {
+    type Value = DescriptionComponent;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        ProjectParser::parse(input)
+            .or(ContextParser::parse(input))
+            .or(KeyValueParser::parse(input))
+            .or(NormalTextParser::parse(input))
+    }
+}
+
+pub struct DescriptionComponentsParser {}
+
+impl Parser for DescriptionComponentsParser {
+    type Value = DescriptionComponents;
+
+    fn parse(input: &str) -> Option<ParserResult<Self::Value>> {
+        let mut description_components = DescriptionComponents::new();
+        let mut remaining = input;
+        while !remaining.is_empty() {
+            if let Some(component) = DescriptionComponentParser::parse(remaining) {
+                remaining = component.remaining;
+                description_components.push(component.value)
+            }
+        }
+        Some(ParserResult {
+            value: description_components,
+            remaining: "",
+        })
+    }
+}
+
 pub trait ComponentExtractor {
     fn extract_components(self) -> Vec<DescriptionComponent>;
 }
 
 impl<'a> ComponentExtractor for &'a str {
-    fn extract_components(self) -> Vec<DescriptionComponent> {
-        lazy_static! {
-            static ref TAG_REGEX : Regex = Regex::new(r"(\+(?P<project>[^\s]+)|@(?P<context>[^\s]+)|(?P<keyvalue>(?P<key>[^\s:]+):(?P<value>[^\s:]+)))").expect("Regex is invalid");
-        }
-
-        let mut remaining_description = self;
-        let mut components = vec![];
-        while !remaining_description.is_empty() {
-            match TAG_REGEX.captures(remaining_description) {
-                Some(captures) => {
-                    let start;
-                    let end;
-                    let component = if let Some(project) = captures.name("project") {
-                        start = project.start() - 1;
-                        end = project.end();
-
-                        DescriptionComponent::Project(String::from(project.as_str()))
-                    } else if let Some(context) = captures.name("context") {
-                        start = context.start() - 1;
-                        end = context.end();
-
-                        DescriptionComponent::Context(String::from(context.as_str()))
-                    } else if let (Some(key), Some(value)) = (captures.name("key"), captures.name("value")) {
-                        start = key.start();
-                        end = value.end();
-
-                        DescriptionComponent::KeyValue(String::from(key.as_str()), String::from(value.as_str()))
-                    } else {
-                        start = remaining_description.len();
-                        end = start;
-
-                        DescriptionComponent::Text(String::from(remaining_description))
-                    };
-
-                    if (start > 0) && (start != remaining_description.len()) {
-                        let text = DescriptionComponent::Text(String::from(&remaining_description[0..start]));
-                        components.push(text);
-                    }
-
-                    components.push(component);
-
-                    remaining_description = &remaining_description[end..];
-                },
-                None => {
-                    components.push(DescriptionComponent::Text(String::from(remaining_description)));
-                    remaining_description = &remaining_description[0..0];
-                }
-            };
-        }
-
-        components
+    fn extract_components(self) -> DescriptionComponents {
+        DescriptionComponentsParser::parse(self)
+            .map_or(DescriptionComponents::new(), |result| result.value)
     }
 }
 
@@ -91,9 +166,119 @@ pub fn description_components_to_string(components: &Vec<DescriptionComponent>) 
 
 #[cfg(test)]
 mod tests {
-    use description_component::ComponentExtractor;
-    use description_component::DescriptionComponent;
-    use description_component::description_components_to_string;
+    use description_component::*;
+
+    #[test]
+    fn project_parser_should_parse_projects() {
+        let project_result = ProjectParser::parse("+Project42 Something else").expect("Must parse.");
+
+        assert_eq!(project_result.value, DescriptionComponent::Project(String::from("Project42")));
+        assert_eq!(project_result.remaining, " Something else");
+    }
+
+    #[test]
+    fn project_parser_should_not_parse_non_projects() {
+        assert!(ProjectParser::parse("+ Project").is_none());
+        assert!(ProjectParser::parse(" +Project").is_none());
+    }
+
+    #[test]
+    fn context_parser_should_parse_contexts() {
+        let context_result = ContextParser::parse("@Context42 Something else").expect("Must parse");
+
+        assert_eq!(context_result.value, DescriptionComponent::Context(String::from("Context42")));
+        assert_eq!(context_result.remaining, " Something else");
+    }
+
+    #[test]
+    fn context_parser_should_not_parse_non_contexts() {
+        assert!(ContextParser::parse("@ Context").is_none());
+        assert!(ContextParser::parse(" @Context").is_none());
+    }
+
+    #[test]
+    fn key_value_parser_should_parse_key_values() {
+        let key_value_result = KeyValueParser::parse("bla:42 Something else").expect("Must parse");
+
+        assert_eq!(key_value_result.value, DescriptionComponent::KeyValue(String::from("bla"), String::from("42")));
+        assert_eq!(key_value_result.remaining, " Something else");
+    }
+
+    #[test]
+    fn key_value_parser_should_not_parse_non_contexts() {
+        assert!(KeyValueParser::parse("key: value").is_none());
+        assert!(KeyValueParser::parse("key :value").is_none());
+        assert!(KeyValueParser::parse("key : value").is_none());
+        assert!(KeyValueParser::parse(" key:value").is_none());
+    }
+
+    #[test]
+    fn text_parser_should_parse_text_without_tags() {
+        let text_result = NormalTextParser::parse("Hello World!").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from("Hello World!")));
+        assert_eq!(text_result.remaining, "");
+    }
+
+    #[test]
+    fn text_parser_should_not_parse_empty_string() {
+        assert!(NormalTextParser::parse("").is_none());
+    }
+
+    #[test]
+    fn text_parser_should_parse_text_before_context() {
+        let text_result = NormalTextParser::parse("Text @Context").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from("Text ")));
+        assert_eq!(text_result.remaining, "@Context");
+    }
+
+    #[test]
+    fn text_parser_should_parse_text_before_project() {
+        let text_result = NormalTextParser::parse("Text +Project").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from("Text ")));
+        assert_eq!(text_result.remaining, "+Project");
+    }
+
+    #[test]
+    fn text_parser_should_parse_text_before_key_value() {
+        let text_result = NormalTextParser::parse("Text key:value").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from("Text ")));
+        assert_eq!(text_result.remaining, "key:value");
+    }
+
+    #[test]
+    fn text_parser_should_parse_space_before_project() {
+        let text_result = NormalTextParser::parse(" +Project").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from(" ")));
+        assert_eq!(text_result.remaining, "+Project");
+    }
+
+    #[test]
+    fn text_parser_should_parse_space_before_context() {
+        let text_result = NormalTextParser::parse(" @Context").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from(" ")));
+        assert_eq!(text_result.remaining, "@Context");
+    }
+
+    #[test]
+    fn text_parser_should_parse_space_before_key_value() {
+        let text_result = NormalTextParser::parse(" key:value").expect("Must parse");
+
+        assert_eq!(text_result.value, DescriptionComponent::Text(String::from(" ")));
+        assert_eq!(text_result.remaining, "key:value");
+    }
+
+    #[test]
+    fn text_parser_should_not_parse_text_when_it_starts_with_a_tag() {
+        assert!(NormalTextParser::parse("@Context").is_none());
+        assert!(NormalTextParser::parse("+Project").is_none());
+        assert!(NormalTextParser::parse("key:value").is_none());
+    }
 
     #[test]
     fn components_extractor_should_extract_components_from_description() {
